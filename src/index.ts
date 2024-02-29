78795df7acf79738741d349c54c6ce1cbe36c3e4
munezeromicha <munezerontaganiramichel@gmail.com>
2024-03-01 01:05:22 +0200

    MyBrand-BE~updates: New Swagger updated!

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,13 @@ import swaggerUI from 'swagger-ui-express';
 // import swaggerSpec from './swagger';
 import passport from './helper/delay';
 import * as Doc from "./mySwagger.json";
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
 app.use(passport.initialize());
 app.use("/api", blogRoutes, commentRoutes,endUser,queriesRouter);
+app.use(cors());
 app.use('/swagger-doc', swaggerUI.serve,swaggerUI.setup(Doc))
 
 export default app;
